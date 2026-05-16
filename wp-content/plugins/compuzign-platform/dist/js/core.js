ef715fb87df35f2f2ad0540c8652283b5de6d78b
Nath Samaraweera <nath@Naths-Mac-mini.local>
2026-05-17 01:29:46 +1000

    Add local frontend build lock file

--- wp-content/plugins/compuzign-platform/dist/js/core.js
+++ wp-content/plugins/compuzign-platform/dist/js/core.js
@@ -1,15 +1,1 @@
-(function () {
-  var app = window.CompuZign || {};
-  var core = {
-    loaded: true,
-    modules: ['core'],
-    init: function () {
-      this.modules.push('core');
-      console.log('CompuZign core initialized');
-    }
-  };
-  window.CompuZign = Object.assign({}, app, core);
-  if (window.CompuZign.init) {
-    window.CompuZign.init();
-  }
-})();
+function n(){console.log("Quote Builder component ready")}const e=window.CompuZign||{},u={loaded:!0,modules:["core"],init(){n(),this.modules.push("core"),console.log("CompuZign core initialized")}};window.CompuZign={...e,...u};var o,i;(i=(o=window.CompuZign).init)==null||i.call(o);
--- wp-content/plugins/compuzign-platform/dist/js/cost-builder.js
+++ wp-content/plugins/compuzign-platform/dist/js/cost-builder.js
@@ -1,16 +1,1 @@
-(function () {
-  var app = window.CompuZign || {};
-  var costBuilder = {
-    loaded: true,
-    services: [],
-    init: function () {
-      console.log('CompuZign Cost Builder module initialized');
-    }
-  };
-  window.CompuZign = Object.assign({}, app, {
-    CostBuilder: costBuilder
-  });
-  if (window.CompuZign.CostBuilder && typeof window.CompuZign.CostBuilder.init === 'function') {
-    window.CompuZign.CostBuilder.init();
-  }
-})();
+const n={loaded:!0,services:[],init(){console.log("CompuZign Cost Builder module initialized")}},d=window.CompuZign||{};window.CompuZign={...d,CostBuilder:n};var i,o;(o=(i=window.CompuZign.CostBuilder)==null?void 0:i.init)==null||o.call(i);
