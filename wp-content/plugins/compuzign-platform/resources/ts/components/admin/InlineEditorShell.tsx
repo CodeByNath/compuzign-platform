77ffd336af837e202b1515bbcd508fc9d8999be8
Nath Samaraweera <nathsamaraweera@gmail.com>
2026-06-21 02:48:57 +1000

    Refine inline editor header and overview card styling

--- wp-content/plugins/compuzign-platform/resources/ts/components/admin/InlineEditorShell.tsx
+++ wp-content/plugins/compuzign-platform/resources/ts/components/admin/InlineEditorShell.tsx
@@ -30,7 +30,15 @@ export function InlineEditorShell({ title, onSave, onCancel, saving, saveErr, is
       <div class="cz-ies__header">
         <div class="cz-ies__nav">
           <button type="button" class="cz-action-shell__back" onClick={handleCancelClick} disabled={saving} aria-label={`Back from ${title}`}>
-            ‹
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
+            </svg>
           </button>
           <span class="cz-ies__title">{title}</span>
         </div>
